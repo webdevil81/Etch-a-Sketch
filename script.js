7d95d76bd4d3877df3eab8a9c63ab133f0872f56
James Goatcher <webdevil81@gmail.com>
2020-09-16 13:23:33 +0100

    Added drawing functionality

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ let dimension = 16;
 for (let i = 0; i < (dimension * dimension); i++) {
     const square = document.createElement('div');
     square.classList.add('start');
+    square.addEventListener('mouseover', () => {
+        square.classList.add('draw');
+    })
+
     container.appendChild(square);
 }
 
